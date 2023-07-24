9469cc027bb6f0b2bbfae6a0f78763ef44f7fcf4
yeoni <ghj0308@daum.net>
2023-07-24 10:04:37 +0900

    커밋 테스트

---
+++ wee☺︎k 02/SeSACWeek2/SeSACWeek2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SeSACWeek2
+//
+//  Created by yeoni on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hello")
+        
+        
+    }
+
+
+}
+

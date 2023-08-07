5c876e99e4df58aba2ef0c93c77432d8c50303cc
yeoni <ghj0308@daum.net>
2023-08-07 12:28:52 +0900

    add gitignore

---
+++ week 04/SeSACWeek4/SeSACWeek4/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SeSACWeek4
+//
+//  Created by yeoni on 2023/08/07.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        callRequest()
+    }
+
+
+    func callRequest() {
+        
+        
+        // https가 아니라 http!!!!!
+        // 오류 뜸
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+}
+

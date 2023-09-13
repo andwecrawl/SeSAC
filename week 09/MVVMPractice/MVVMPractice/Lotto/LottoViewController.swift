6ef3a20875403b1cd1795f319f0eacfba8b49d58
yeoni <ghj0308@daum.net>
2023-09-14 01:15:16 +0900

    LottoView와 LottoVC 연결

--- week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewController.swift
+++ week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewController.swift
@@ -8,11 +8,44 @@
 import UIKit
 
 class LottoViewController: UIViewController {
+    
+    let mainView = LottoView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureView()
     }
+    
+    func configureView() {
+        mainView.pickerView.delegate = self
+        mainView.pickerView.dataSource = self
+        
     }
     
+    
+}
+
+extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("clicked!! \(component), \(row)")
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row)"
+    }
+
+
 }

2c62b326e2aad21badf607429add6e715c5a56ff
yeoni <ghj0308@daum.net>
2023-09-14 01:45:27 +0900

    viewModel 구현

--- week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewController.swift
+++ week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LottoViewController: UIViewController {
     
     let mainView = LottoView()
+    let viewModel = LottoViewModel()
     
     override func loadView() {
         self.view = mainView
@@ -19,11 +20,59 @@ class LottoViewController: UIViewController {
         super.viewDidLoad()
         
         configureView()
+        bindData()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.viewModel.fetchLottoAPI(drwNo: 1084)
+        }
+        
     }
     
     func configureView() {
         mainView.pickerView.delegate = self
         mainView.pickerView.dataSource = self
+        
+        mainView.chooseButton.addTarget(self, action: #selector(chooseButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func chooseButtonClicked() {
+        self.viewModel.fetchLottoAPI(drwNo: Int.random(in: 1000...1083))
+        print("clicked!!")
+    }
+    
+    
+    func bindData() {
+        viewModel.num1.bind { value in
+            self.mainView.firstLabel.text = "\(value)"
+        }
+        
+        viewModel.num2.bind { value in
+            self.mainView.secondLabel.text = "\(value)"
+        }
+        
+        viewModel.num3.bind { value in
+            self.mainView.thirdLabel.text = "\(value)"
+        }
+        
+        viewModel.num4.bind { value in
+            self.mainView.fourthLabel.text = "\(value)"
+        }
+        
+        viewModel.num5.bind { value in
+            self.mainView.fifthLabel.text = "\(value)"
+        }
+        
+        viewModel.num6.bind { value in
+            self.mainView.sixthLabel.text = "\(value)"
+        }
+        
+        viewModel.num7.bind { value in
+            self.mainView.seventhLabel.text = "\(value)"
+        }
+        
+        viewModel.price.bind { value in
+            self.mainView.priceLabel.text = "당첨금: \(value)원"
+        }
         
     }
     
@@ -34,18 +83,18 @@ extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 100
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("clicked!! \(component), \(row)")
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return "\(row)"
     }
-
-
+    
+    
 }
---
+++ week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  LottoViewModel.swift
+//  MVVMPractice
+//
+//  Created by yeoni on 2023/09/14.
+//
+
+import Foundation
+
+class LottoViewModel {
+    let num1 = Observable(1)
+    let num2 = Observable(2)
+    let num3 = Observable(3)
+    let num4 = Observable(4)
+    let num5 = Observable(5)
+    let num6 = Observable(6)
+    let num7 = Observable(7)
+    let price = Observable("당첨금")
+    
+    func format(_ num: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(for: num) ?? ""
+    }
+    
+    func fetchLottoAPI(drwNo: Int) {
+        
+        APIHelper.shared.requestLotto(drwNo: drwNo) { lotto, message in
+            guard let lotto else {
+                print(message)
+                return
+            }
+            
+            self.num1.value = lotto.drwtNo1
+            self.num2.value = lotto.drwtNo2
+            self.num3.value = lotto.drwtNo3
+            self.num4.value = lotto.drwtNo4
+            self.num5.value = lotto.drwtNo5
+            self.num6.value = lotto.drwtNo6
+            self.num7.value = lotto.bnusNo
+            self.price.value = self.format(lotto.totSellamnt)
+        }
+    }
+}

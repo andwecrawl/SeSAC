841616e436d66efbd50a2b147bbb2775fee3b205
yeoni <ghj0308@daum.net>
2023-09-14 01:13:42 +0900

    프로젝트 생성 후 main Storyboard 제거

---
+++ week 09/MVVMPractice/MVVMPractice/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by yeoni on 2023/09/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        setConstraints()
+    }
+    
+    
+    func configureView() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+    
+    
+    
+}
+
---
+++ week 09/MVVMPractice/MVVMPractice/Lotto/LottoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LottoViewController.swift
+//  MVVMPractice
+//
+//  Created by yeoni on 2023/09/13.
+//
+
+import UIKit
+
+class LottoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+    }
+    }
+    
+}

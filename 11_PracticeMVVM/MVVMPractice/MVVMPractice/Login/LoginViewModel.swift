59ffe7cb57cf2266d755091f8f3c9d4700c997d7
yeoni <ghj0308@daum.net>
2023-09-19 00:24:52 +0900

    checkValidation 함수 로직 수정

--- 11_PracticeMVVM/MVVMPractice/MVVMPractice/Login/LoginViewModel.swift
+++ 11_PracticeMVVM/MVVMPractice/MVVMPractice/Login/LoginViewModel.swift
@@ -9,18 +9,39 @@ import Foundation
 
 class LoginViewModel {
     
-    var id = Observable("id")
-    var password = Observable("password")
-    var nickname = Observable("nickname")
-    var inviteCode = Observable("12345678")
+    var id = Observable("")
+    var password = Observable("")
+    var nickname = Observable("")
+    var inviteCode = Observable("")
     var isVailed = Observable(false)
     
-    func checkValidation() {
-        if id.value.count > 5 && password.value.count > 8
-            && inviteCode.value.count > 5 {
+    func checkValidation(completion: @escaping (String) -> ()) {
+        var checkID = false
+        var checkPassword = false
+        var checkCode = false
+        
+        if id.value.count > 5 && id.value.contains("@") {
+            checkID = true
+        }
+        if password.value.count > 5 && password.value.count < 11 {
+            checkPassword = true
+        }
+        if inviteCode.value.count > 5 {
+            checkCode = true
+        }
+        
+        if checkID && checkPassword && checkCode {
+            completion("환영합니다!")
             isVailed.value = true
         } else {
             isVailed.value = false
+            if !checkID {
+                completion("id를 6자 이상, @를 포함하여 입력해 주세요!")
+            } else if !checkPassword {
+                completion("비밀번호는 6자 이상, 10자 이하로 입력해 주세요")
+            } else if !checkCode {
+                completion("추천 코드를 입력해 주세요!")
+            }
         }
     }
     

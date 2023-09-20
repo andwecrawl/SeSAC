0be286848b037b56ff3da30616eab1b0a9d7fc6e
yeoni <ghj0308@daum.net>
2023-09-20 16:41:41 +0900

    VC에서 ViewModel로 로직 분리

--- 13_getBeer/Cheers!/Cheers!/BeersViewModel.swift
+++ 13_getBeer/Cheers!/Cheers!/BeersViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class BeerViewModel {
+    
+    func request(api: Router) {
+        Network.shared.request(type: Beer.self, api: api) { response in
+            switch response {
+            case .success(let success):
+                dump(success)
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
+}

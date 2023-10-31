2e56bea6212575c17163480f2c5c6633935a6ae5
yeoni <ghj0308@daum.net>
2023-11-01 03:03:20 +0900

    feat: pickerView method 추가

--- 14_PracticeRx/PracticeRxSwift/PracticeRxSwift/RxPickerViewController.swift
+++ 14_PracticeRx/PracticeRxSwift/PracticeRxSwift/RxPickerViewController.swift
@@ -24,6 +24,8 @@ class RxPickerViewController: UIViewController {
         return view
     }()
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,7 +35,7 @@ class RxPickerViewController: UIViewController {
     }
     
     func setConstraints() {
-        
+        view.backgroundColor = .white
         [pickerView1, pickerView2, pickerView3]
             .forEach { pickerView in
                 stackView.addArrangedSubview(pickerView)
@@ -47,8 +49,40 @@ class RxPickerViewController: UIViewController {
     
     func configureView() {
         
+        Observable.just([1, 2, 3])
+            .bind(to: pickerView1.rx.itemTitles) { _, item in
+                return "\(item)"
+            }
+            .disposed(by: disposeBag)
+        
+        Observable.just([1, 2, 3])
+            .bind(to: pickerView2.rx.itemAttributedTitles) {
+                _, item in
+                return NSAttributedString(string: "\(item)", attributes: [NSAttributedString.Key.foregroundColor: UIColor.green, NSAttributedString.Key.underlineStyle: NSUnderlineStyle.double.rawValue])
+            }
+            .disposed(by: disposeBag)
+        
+        pickerView2.rx.modelSelected(Int.self)
+            .subscribe { models in
+                print("models selected 2: \(models)")
+            }
+            .disposed(by: disposeBag)
         
         
+        Observable.just([UIColor.red, UIColor.green, UIColor.blue])
+            .bind(to: pickerView3.rx.items) {
+                _, item, _ in
+                let view = UIView()
+                view.backgroundColor = item
+                return view
+            }
+            .disposed(by: disposeBag)
+        
+        pickerView3.rx.modelSelected(UIColor.self)
+            .subscribe { models in
+                print("models selected 3: \(models)")
+            }
+            .disposed(by: disposeBag)
     }
     
 }

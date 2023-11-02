04db518b049e56b7ad7c679fbd32a3c0548d454c
yeoni <ghj0308@daum.net>
2023-11-03 03:50:53 +0900

    edit: viewModel 분리

--- 15_SeSACRxThreads/SeSACRxThreads/SignUpOptional/Phone/PhoneViewController.swift
+++ 15_SeSACRxThreads/SeSACRxThreads/SignUpOptional/Phone/PhoneViewController.swift
@@ -15,11 +15,11 @@ class PhoneViewController: UIViewController {
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
     
-    let phone = BehaviorSubject(value: "010")
-    let isEnabled = BehaviorSubject(value: false)
     let buttonColor = BehaviorSubject(value: UIColor.red)
     
     let disposeBag = DisposeBag()
+    
+    let viewModel = PhoneViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,7 +56,7 @@ class PhoneViewController: UIViewController {
     
     
     func bind() {
-        isEnabled
+        viewModel.isEnabled
             .bind(to: nextButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
@@ -69,60 +69,23 @@ class PhoneViewController: UIViewController {
             .bind(to: phoneTextField.layer.rx.borderColor)
             .disposed(by: disposeBag)
         
-        phone
-            .bind(to: phoneTextField.rx.text)
-            .disposed(by: disposeBag)
-        
-        
-        phone
-            .map({ $0.count > 12 })
-            .subscribe(with: self) { owner, value in
-                
+        viewModel.isEnabled
+            .bind(with: self) { owner, value in
                 let color = value ? UIColor.blue : UIColor.red
                 owner.buttonColor.onNext(color)
-                owner.isEnabled.onNext(value)
-                owner.isEnabled.on(.next(value))
-                
             }
             .disposed(by: disposeBag)
         
-        
-        /*
-        phone
-            .map({ $0.count > 10 })
-            .withUnretained(self)
-            .subscribe { object, value in
-                let color = value ? UIColor.blue : UIColor.red
-                object.buttonColor.onNext(color)
-                object.isEnabled.onNext(value)
-                object.isEnabled.on(.next(value))
-            }
+        viewModel.phone
+            .bind(to: phoneTextField.rx.text)
             .disposed(by: disposeBag)
-        
-        // 메모리 누수가 날 수 있음 :3,,
-        phone
-            .map({ $0.count > 10 })
-            .subscribe { value in
-                let color = value ? UIColor.blue : UIColor.red
-                self.buttonColor.onNext(color)
-                self.isEnabled.onNext(value)
-                self.isEnabled.on(.next(value))
-            }
-            .disposed(by: disposeBag)
-         */
-        
+
         phoneTextField.rx.text.orEmpty
-            .subscribe(with: self, onNext: { object, value in
+            .subscribe(with: self, onNext: { owner, value in
                 let result = value.formated(by: "###-####-####")
-                object.phone.onNext(result)
+                owner.viewModel.phone.onNext(result)
             })
             .disposed(by: disposeBag)
-        
-//        phoneTextField.rx.text.orEmpty
-//            .subscribe { value in
-//                let result = value.formated(by: "###-####-####")
-//                self.phone.onNext(result)
-//            }
         
     }
 

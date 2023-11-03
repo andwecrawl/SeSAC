6392c1c31f87337b66c4a1a0c7ac4bb193ed4666
yeoni <ghj0308@daum.net>
2023-11-03 19:14:02 +0900

    feat: SignInViewModel

--- 15_SeSACRxThreads/SeSACRxThreads/SignIn/SignInViewModel.swift
+++ 15_SeSACRxThreads/SeSACRxThreads/SignIn/SignInViewModel.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class SignInViewModel {
     
+    var email = BehaviorSubject(value: "")
+    var password = BehaviorSubject(value: "")
     
+    var isEnable = BehaviorSubject(value: false)
+    
+    lazy var validation = Observable.combineLatest(email, password) { email, password in
+        return email.count > 5 && password.count > 5
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    init() {
+        validation
+            .bind(to: isEnable)
+            .disposed(by: disposeBag)
+    }
     
 }

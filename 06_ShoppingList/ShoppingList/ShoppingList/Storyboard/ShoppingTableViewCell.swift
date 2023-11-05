ec8f38c4c9b9089b2fe3c2415533601efd24aa31
yeoni <ghj0308@daum.net>
2023-11-05 15:05:35 +0900

    rename: Storyboard 폴더 정리
    리팩토링도 조금 함 ,,

--- 06_ShoppingList/ShoppingList/ShoppingList/Storyboard/ShoppingTableViewCell.swift
+++ 06_ShoppingList/ShoppingList/ShoppingList/Storyboard/ShoppingTableViewCell.swift
@@ -6,25 +6,32 @@
 //
 
 import UIKit
+import RxSwift
 
 class ShoppingTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "shoppingTableViewCell"
     
     @IBOutlet weak var checkboxButton: UIButton!
     @IBOutlet weak var listLabel: UILabel!
     @IBOutlet weak var starButton: UIButton!
     
+    var stuff: Stuff?
     
-    func configurateCell(row: Stuff) {
+    let disposeBag = DisposeBag()
+    
+    func configurateCell() {
+        guard let stuff else { return }
         
-        listLabel.text = row.name
+        listLabel.text = stuff.name
         
-        if row.checked {
+        if stuff.checked {
             checkboxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
         } else {
             checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
         }
         
-        if row.liked {
+        if stuff.liked {
             starButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
         } else {
             starButton.setImage(UIImage(systemName: "star"), for: .normal)
--- 06_ShoppingList/ShoppingList/ShoppingList/Storyboard/ShoppingTableViewController.swift
+++ 06_ShoppingList/ShoppingList/ShoppingList/Storyboard/ShoppingTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ShoppingTableViewController: UITableViewController {
     
@@ -15,19 +17,24 @@ class ShoppingTableViewController: UITableViewController {
         }
     }
     
+    var data = Shopping()
+    
+    lazy var list = BehaviorSubject(value: data.list)
+    
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
     
     var somethingToBuy: String = ""
     
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         designHeader()
-        
     }
     
+
     
     @IBAction func textingTextField(_ sender: UITextField) {
         guard let text = sender.text else {
@@ -49,13 +56,13 @@ class ShoppingTableViewController: UITableViewController {
     @IBAction func addButtonTapped(_ sender: UIButton) {
         
         if somethingToBuy.isEmpty {
-            giveAlert(title: "추가할 물건을 적어 주세요!", message: "")
+            sendOneSideAlert(title: "추가할 물건을 적어 주세요!", message: "")
             return
         }
         
         // 이미 같은 물건이 list 안에 있을 때 alert 기능
         if shopping.containedList(str: somethingToBuy) {
-            giveAlert(title: "이미 리스트에 존재하는 물품입니다!", message: "")
+            sendOneSideAlert(title: "이미 리스트에 존재하는 물품입니다!", message: "")
             return
         }
     
@@ -63,37 +70,6 @@ class ShoppingTableViewController: UITableViewController {
         shopping.list.insert(thing, at: 0)
         inputTextField.text = ""
         somethingToBuy = ""
-        tableView.reloadData()
-        
-    }
-    
-    
-    // cell 갯수
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shopping.list.count
-    }
-    
-    
-    // section Number
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    // cell design
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingTableViewCell") as! ShoppingTableViewCell
-        
-        let row = shopping.list[indexPath.row] // Stuff
-        
-        cell.configurateCell(row: row)
-        cell.starButton.tag = indexPath.row
-        cell.starButton.addTarget(self, action: #selector(starButtonTapped), for: .touchUpInside)
-        
-        cell.checkboxButton.tag = indexPath.row
-        cell.checkboxButton.addTarget(self, action: #selector(checkboxButtonTapped), for: .touchUpInside)
-        return cell
         
     }
     
@@ -103,6 +79,48 @@ class ShoppingTableViewController: UITableViewController {
     
     @objc func checkboxButtonTapped(_ sender: UIButton) {
         shopping.list[sender.tag].checked.toggle()
+    }
+    
+    func designHeader() {
+        inputTextField.placeholder = "무엇을 구매하실 건가요?"
+        addButton.tintColor = .black
+        addButton.backgroundColor = .systemGray5
+        
+        self.tableView.rowHeight = 55
+    }
+    
+    func sendOneSideAlert(title: String, message: String = "") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okay = UIAlertAction(title: "okay", style: .default, handler: nil)
+        alert.addAction(okay)
+        present(alert, animated: true)
+    }
+    
+}
+
+
+extension ShoppingTableViewController {
+    
+    // cell 갯수
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shopping.list.count
+    }
+    
+    // cell design
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingTableViewCell") as! ShoppingTableViewCell
+        
+        let row = shopping.list[indexPath.row] // Stuff
+        
+        cell.stuff = row
+        cell.starButton.tag = indexPath.row
+        cell.starButton.addTarget(self, action: #selector(starButtonTapped), for: .touchUpInside)
+        
+        cell.checkboxButton.tag = indexPath.row
+        cell.checkboxButton.addTarget(self, action: #selector(checkboxButtonTapped), for: .touchUpInside)
+        return cell
+        
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -117,19 +135,5 @@ class ShoppingTableViewController: UITableViewController {
         shopping.list.remove(at: indexPath.row)
     }
     
-    
-    
-    func designHeader() {
-        inputTextField.placeholder = "무엇을 구매하실 건가요?"
-        addButton.tintColor = .black
-        addButton.backgroundColor = .systemGray5
-    }
-    
-    func giveAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okay = UIAlertAction(title: "okay", style: .default, handler: nil)
-        alert.addAction(okay)
-        present(alert, animated: true)
-    }
     
 }

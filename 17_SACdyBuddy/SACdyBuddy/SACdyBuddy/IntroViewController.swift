f7857500567b8144838b2ee5aa0a30dfd3c78fb7
yeoni <ghj0308@daum.net>
2023-11-09 20:40:36 +0900

    feat: 첫 화면 UI 구성 완료

--- 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/IntroViewController.swift
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/IntroViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 enum VCType {
     case email
@@ -15,16 +17,81 @@ enum VCType {
 
 class IntroViewController: BaseViewController {
     
+    let informationLabel = {
+        let label = UILabel()
+        label.text = "새싹 동료들과의 소통,\n모두 새싹모음에서"
+        label.font = .systemFont(ofSize: 28, weight: .semibold)
+        return label
+    }()
+    
+    let descriptionLabel = {
+        let label = UILabel()
+        label.text = "왠지 무서운 오픈라운지 대신\n간편하게 만나요!"
+        label.font = .systemFont(ofSize: 16, weight: .light)
+        label.textColor = .systemGray2
+        return label
+    }()
+    
+    let nextButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemGreen
+        config.title = "시작하기"
+        button.configuration = config
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bind()
+    }
     
     override func configureHierarchy() {
         super.configureHierarchy()
+        
+        [
+            informationLabel,
+            nextButton,
+            descriptionLabel
+        ]
+            .forEach({ view.addSubview($0) })
+        
     }
     
     override func setConstraints() {
-        <#code#>
+        
+        informationLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(80)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.height.equalTo(52)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(informationLabel.snp.bottom).offset(24)
+            make.horizontalEdges.equalTo(informationLabel)
+        }
     }
     
     override func configureView() {
+        informationLabel.numberOfLines = 0
+        informationLabel.setLineSpacing(lineSpacing: 6)
+        
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.setLineSpacing(lineSpacing: 5)
+    }
+    
+    func bind() {
+        
+        nextButton.rx.tap
+            .bind { _ in
+                let vc = 
+            }
         
     }
     

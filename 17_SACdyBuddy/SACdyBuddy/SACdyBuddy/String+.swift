63a33f56c0d779e660c628eb8bd2d67625be36f1
yeoni <ghj0308@daum.net>
2023-11-09 20:40:19 +0900

    feat: String 행간 extension method 추가

---
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/String+.swift
@@ -0,0 +1,36 @@
+//
+//  String+.swift
+//  SACdyBuddy
+//
+//  Created by yeoni on 2023/11/09.
+//
+
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(kernValue: Double = 0.0,
+                        lineSpacing: CGFloat = 0.0,
+                        lineHeightMultiple: CGFloat = 0.0) {
+        guard let labelText = self.text else { return }
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+        
+        let attributedString: NSMutableAttributedString
+        if let labelAttributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelAttributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+        
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                      value: paragraphStyle,
+                                      range: NSMakeRange(0, attributedString.length))
+        attributedString.addAttribute(NSAttributedString.Key.kern,
+                                      value: kernValue,
+                                      range: NSMakeRange(0, attributedString.length))
+        
+        self.attributedText = attributedString
+    }
+}

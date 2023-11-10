74cb86e16a14b0d33aec8c6c4b7ebaa3de658608
yeoni <ghj0308@daum.net>
2023-11-10 22:19:34 +0900

    feat: JoinViewModel

---
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/Onboarding/JoinViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  JoinViewModel.swift
+//  SACdyBuddy
+//
+//  Created by yeoni on 2023/11/10.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class JoinViewModel {
+    
+    let isValid = BehaviorSubject(value: false)
+    
+    struct Input {
+    }
+    
+    struct Output {
+        
+        
+    }
+    
+    func translate(input: Input) -> Output {
+        
+        return Output()
+    }
+    
+    
+}

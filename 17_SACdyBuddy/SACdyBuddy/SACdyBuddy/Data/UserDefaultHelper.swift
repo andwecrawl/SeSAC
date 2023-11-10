c1d6f63472a0a9dceca95037c437f6626e10f31d
yeoni <ghj0308@daum.net>
2023-11-10 17:00:35 +0900

    feat: UserDefaultHelper

---
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/Data/UserDefaultHelper.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaultHelper.swift
+//  SACdyBuddy
+//
+//  Created by yeoni on 2023/11/09.
+//
+
+import Foundation
+
+class UserDefaultsHelper {
+    
+    static let shared = UserDefaultsHelper()
+    
+    private init() { }
+    
+    let userDefaults = UserDefaults.standard
+    
+    var haveBeenBefore: Bool {
+        get {
+            return userDefaults.bool(forKey: "haveBeenBefore")
+        }
+        set {
+            userDefaults.set(newValue, forKey: "haveBeenBefore")
+        }
+    }
+    
+    
+}

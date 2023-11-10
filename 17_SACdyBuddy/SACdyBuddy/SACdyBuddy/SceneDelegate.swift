c1bf5dcc41c98fd21cdf554021331e0240503ea8
yeoni <ghj0308@daum.net>
2023-11-10 17:02:32 +0900

    feat: 처음 실행하는 유저와 아닌 유저 구분

--- 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/SceneDelegate.swift
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let vc = IntroViewController()
+        let vc = UserDefaultsHelper.shared.haveBeenBefore ? IntroViewController() : LoginViewController()
         window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
     }

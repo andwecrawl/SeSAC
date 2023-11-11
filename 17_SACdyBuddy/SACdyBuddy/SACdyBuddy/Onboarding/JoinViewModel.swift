37d705675231b7981153fbee7eac5560508e787b
yeoni <ghj0308@daum.net>
2023-11-11 19:11:29 +0900

    feat: input-output Pattern 적용

--- 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/Onboarding/JoinViewModel.swift
+++ 17_SACdyBuddy/SACdyBuddy/SACdyBuddy/Onboarding/JoinViewModel.swift
@@ -11,20 +11,44 @@ import RxCocoa
 
 class JoinViewModel {
     
-    let isValid = BehaviorSubject(value: false)
+    var VCType: VCType?
+    let disposeBag = DisposeBag()
     
     struct Input {
+        let userInput: ControlProperty<String>
+        let tap: ControlEvent<Void>
     }
     
     struct Output {
         
+        let isValid: BehaviorSubject<Bool>
+        let tap: ControlEvent<Void>
         
     }
     
-    func translate(input: Input) -> Output {
+    func translate(input: Input) -> Output? {
         
-        return Output()
+        guard let VCType else { return nil }
+        
+        let isValid = BehaviorSubject(value: false)
+        
+        input.userInput
+            .map { str in
+                switch VCType {
+                case .email:
+                    return (str.range(of: RegexType.email.rawValue, options: .regularExpression) != nil)
+                case .password:
+                    return (str.range(of: RegexType.password.rawValue, options: .regularExpression) != nil)
+                default:
+                    return false
+                }
+            }
+            .bind(to: isValid)
+            .disposed(by: disposeBag)
+        
+        return Output(
+            isValid: isValid,
+            tap: input.tap
+        )
     }
-    
-    
 }

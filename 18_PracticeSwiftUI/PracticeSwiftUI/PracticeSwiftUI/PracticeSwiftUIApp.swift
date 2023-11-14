f0bbf607cdba9f4455197e15bd003a3ee8456f27
yeoni <ghj0308@daum.net>
2023-11-14 16:49:23 +0900

    feat: PracticeSwiftUI

---
+++ 18_PracticeSwiftUI/PracticeSwiftUI/PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by yeoni on 2023/11/14.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2a293b81bfbbaab97d96926a01eb2442fd055775
yeoni <ghj0308@daum.net>
2023-11-16 04:41:41 +0900

    feat: 핸드폰번호 정규식 추가

--- 17_BMob/Bmob/BMob/Data/RegexType.swift
+++ 17_BMob/Bmob/BMob/Data/RegexType.swift
@@ -11,4 +11,5 @@ import Foundation
 enum RegexType: String {
     case email = #"^([a-zA-Z0-9._-])+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$"#
     case password = #"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$"#
+    case phoneNumber = #"[0-9]{3}\-[0-9]{4}\-[0-9]{4}"#
 }

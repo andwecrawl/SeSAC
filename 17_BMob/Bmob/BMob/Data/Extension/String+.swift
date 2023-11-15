c9ae05d374cce5910168a398093607d2432dcd7c
yeoni <ghj0308@daum.net>
2023-11-16 04:04:18 +0900

    feat: String Extension

---
+++ 17_BMob/Bmob/BMob/Data/Extension/String+.swift
@@ -0,0 +1,43 @@
+//
+//  String+.swift
+//  BMob
+//
+//  Created by yeoni on 2023/11/16.
+//
+
+import Foundation
+
+extension String {
+    var decimalFilteredString: String {
+        return String(unicodeScalars.filter(CharacterSet.decimalDigits.contains))
+    }
+    
+    func formated(by patternString: String) -> String {
+        let digit: Character = "#"
+ 
+        let pattern: [Character] = Array(patternString)
+        let input: [Character] = Array(self.decimalFilteredString)
+        var formatted: [Character] = []
+ 
+        var patternIndex = 0
+        var inputIndex = 0
+ 
+        while inputIndex < input.count {
+            let inputCharacter = input[inputIndex]
+ 
+            guard patternIndex < pattern.count else { break }
+ 
+            switch pattern[patternIndex] == digit {
+            case true:
+                formatted.append(inputCharacter)
+                inputIndex += 1
+            case false:
+                formatted.append(pattern[patternIndex])
+            }
+ 
+            patternIndex += 1
+        }
+ 
+        return String(formatted)
+    }
+}

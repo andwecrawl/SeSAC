74d6c212de03bd5546872fc04e632ad3152bcca3
yeoni <ghj0308@daum.net>
2023-11-16 21:41:14 +0900

    feat: LSLPModel

---
+++ 17_BMob/Bmob/BMob/Network/LSLPModel.swift
@@ -0,0 +1,23 @@
+//
+//  LSLPModel.swift
+//  BMob
+//
+//  Created by yeoni on 2023/11/16.
+//
+
+import Foundation
+
+struct Model: Encodable {
+    let email: String
+    let password: String
+    let phoneNumber: String
+}
+
+struct LoginData: Encodable {
+    let email: String
+    let password: String
+}
+
+struct EmailValidation: Encodable {
+    let email: String
+}

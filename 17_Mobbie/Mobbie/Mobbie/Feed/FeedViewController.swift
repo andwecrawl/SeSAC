312ec3dc682205ce9305e2b25234b565cc220cc2
yeoni <ghj0308@daum.net>
2023-11-17 22:31:14 +0900

    feat: FeedVIewController

--- 17_Mobbie/Mobbie/Mobbie/Feed/FeedViewController.swift
+++ 17_Mobbie/Mobbie/Mobbie/Feed/FeedViewController.swift
@@ -9,5 +9,21 @@ import UIKit
 
 class FeedViewController: BaseViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
     
 }

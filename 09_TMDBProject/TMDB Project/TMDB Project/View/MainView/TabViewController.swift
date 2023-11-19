b2b24c19cef9e128ca4ceb879807267ec0f92718
yeoni <ghj0308@daum.net>
2023-11-19 19:52:46 +0900

    feat: TabViewController

---
+++ 09_TMDBProject/TMDB Project/TMDB Project/View/MainView/TabViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TabViewController.swift
+//  TMDB Project
+//
+//  Created by yeoni on 2023/11/19.
+//
+
+import UIKit
+import SnapKit
+
+class TabViewController: BaseViewController {
+    
+    let tableView = {
+        let view = UITableView(frame: .zero)
+        view.register(TrendTableViewCell.self, forCellReuseIdentifier: TrendTableViewCell.identifier)
+        view.register(PersonTableViewCell.self, forCellReuseIdentifier: PersonTableViewCell.identifier)
+        return view
+    }()
+    
+    override func setConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+    }
+    
+    
+}

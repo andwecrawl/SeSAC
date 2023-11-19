271190b6b279f8a9b65449551a17fbb110c5bbab
yeoni <ghj0308@daum.net>
2023-11-19 21:11:42 +0900

    feat: Trends index

--- 09_TMDBProject/TMDB Project/TMDB Project/Module/API/APIManager/APIManager.swift
+++ 09_TMDBProject/TMDB Project/TMDB Project/Module/API/APIManager/APIManager.swift
@@ -11,6 +11,42 @@ import Alamofire
 
 enum Trends: String {
     case all, movie, tv, person
+    
+    var index: Int {
+        get {
+            switch self {
+            case .all:
+                return 0
+            case .movie:
+                return 1
+            case .tv:
+                return 2
+            case .person:
+                return 3
+            }
+        }
+    }
+    
+    static func caseAtIndex(_ index: Int) -> Trends? {
+        switch index {
+        case 0:
+            return .all
+        case 1:
+            return .movie
+        case 2:
+            return .tv
+        case 3:
+            return .person
+        default:
+            return nil
+        }
+    }
+    
+    mutating func setIndex(_ newIndex: Int) {
+        if let newCase = Trends.caseAtIndex(newIndex) {
+            self = newCase
+        }
+    }
 }
 
 class TMDBManager {

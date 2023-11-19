221b3a828f07979f3581d160148b08780da22e6b
yeoni <ghj0308@daum.net>
2023-11-19 21:10:24 +0900

    edit: 필요없는 데이터 삭제

--- 09_TMDBProject/TMDB Project/TMDB Project/Module/Data/Person.swift
+++ 09_TMDBProject/TMDB Project/TMDB Project/Module/Data/Person.swift
@@ -65,43 +65,18 @@ struct People: Decodable {
 
 // MARK: - KnownFor
 struct KnownFor: Decodable {
-    let adult: Bool
-    let backdropPath: String?
     let id: Int
     let title: String?
-    let originalLanguage: String
     let originalTitle: String?
     let overview: String
-    let posterPath: String?
-    let mediaType: MediaType
-    let genreIDS: [Int]
-    let popularity: Double
-    let releaseDate: String?
-    let video: Bool?
-    let voteAverage: Double
-    let voteCount: Int
-    let name, originalName, firstAirDate: String?
-    let originCountry: [String]?
+    let name, originalName: String?
 
     enum CodingKeys: String, CodingKey {
-        case adult
-        case backdropPath = "backdrop_path"
         case id, title
-        case originalLanguage = "original_language"
         case originalTitle = "original_title"
         case overview
-        case posterPath = "poster_path"
-        case mediaType = "media_type"
-        case genreIDS = "genre_ids"
-        case popularity
-        case releaseDate = "release_date"
-        case video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
         case name
         case originalName = "original_name"
-        case firstAirDate = "first_air_date"
-        case originCountry = "origin_country"
     }
 }
 

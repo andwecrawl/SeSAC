edadbce81afe0d24ffd43c5d729cfd291b9e667c
yeoni <ghj0308@daum.net>
2023-12-02 18:37:17 +0900

    edit: update UI

--- 09_TMDBProject/TMDB Project/TMDB Project/View/RecommendationView/Cell/RecommendationCollectionViewCell.swift
+++ 09_TMDBProject/TMDB Project/TMDB Project/View/RecommendationView/Cell/RecommendationCollectionViewCell.swift
@@ -14,14 +14,36 @@ class RecommendationCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bgImageView: UIImageView!
     @IBOutlet weak var posterImageView: UIImageView!
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var movie: Result?
     var posterPath: String?
     
     // bgView와 posterImageView에 모두 posterImage를 넣음 댐~~
     // 아님 bgview에 정말 backdropPath를 넣어도 되고~~!!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = Color.background.color
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 10
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        bgImageView.image = UIImage(named: "test")
+        titleLabel.text = "타이틀이에용"
+        descriptionLabel.text = "설명이에용"
+    }
     
     func configureCell() {
-        guard let posterPath else {
+        guard let movie else { return }
+        guard let posterPath = movie.posterPath else {
             bgImageView.backgroundColor = .gray
             posterImageView.image = UIImage(named: "noImage")
             return
@@ -31,6 +53,16 @@ class RecommendationCollectionViewCell: UICollectionViewCell {
         let url = URL.makeImageURL(imagePath: posterPath)
         bgImageView.kf.setImage(with: url)
         posterImageView.kf.setImage(with: url)
+        
+        if let title = movie.title {
+            titleLabel.text = title
+        } else if let title = movie.originalTitle {
+            titleLabel.text = title
+        } else if let title = movie.originalName {
+            titleLabel.text = title
+        }
+        
+        descriptionLabel.text = movie.descriptions
     }
 
 }

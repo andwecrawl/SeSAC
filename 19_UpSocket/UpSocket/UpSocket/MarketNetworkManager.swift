0f98b307af2c7125daca64be3481a2f80c2028aa
yeoni <ghj0308@daum.net>
2023-12-27 17:06:23 +0900

    feat: UpSocket Project

---
+++ 19_UpSocket/UpSocket/UpSocket/MarketNetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  MarketManager.swift
+//  UpSocket
+//
+//  Created by yeoni on 12/27/23.
+//
+
+import Foundation
+
+class MarketNetworkManager {
+    
+    
+    
+    
+}
---
+++ 19_UpSocket/UpSocket/UpSocket/UpSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpSocketApp.swift
+//  UpSocket
+//
+//  Created by yeoni on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct UpSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

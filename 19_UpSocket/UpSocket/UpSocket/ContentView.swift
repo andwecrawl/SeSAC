3a05cdd51045a4563621c818f994d64a70f5810c
yeoni <ghj0308@daum.net>
2023-12-29 11:04:48 +0900

    feat: add feedUI

--- 19_UpSocket/UpSocket/UpSocket/ContentView.swift
+++ 19_UpSocket/UpSocket/UpSocket/ContentView.swift
@@ -7,18 +7,48 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+         ㄹ
+                coinCell(coinName: "코인이름", coinEngName: "코인이름", comparsion: "현재가", cost: "전일대비", payment: "거래대금")
+                
+            }
+            .padding()
+            .navigationTitle("upsockets")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct coinCell: View {
+    @State var coinName: String
+    @State var coinEngName: String
+    @State var comparsion: String
+    @State var cost: String
+    @State var payment: String
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            VStack(alignment: .leading) {
+                Text(coinName)
+                    .font(.subheadline.bold())
+                Text(coinEngName)
+                    .font(.caption)
+            }
+            .padding()
+            Group {
+                Text("현재가")
+                Text("전일대비")
+                Text("거래 대금")
+            }
+            .padding(10)
+            .font(.subheadline)
+        }
+    }
 }

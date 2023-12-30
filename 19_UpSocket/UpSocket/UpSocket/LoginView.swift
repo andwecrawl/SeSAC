b3370e0f1d0afcc7d3168854b6ff18b2dd2188ea
yeoni <ghj0308@daum.net>
2023-12-30 20:32:21 +0900

    feat: LoginView

---
+++ 19_UpSocket/UpSocket/UpSocket/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/1/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+    
+        Text("Hello")
+//        Button {
+//            <#code#>
+//        } label: {
+//            <#code#>
+//        }
+
+    }
+}
+
+#Preview {
+    LoginView()
+}

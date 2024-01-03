b227319307e769db97c266f07bb0706edbbfec7d
yeoni <ghj0308@daum.net>
2024-01-04 04:23:18 +0900

    feat: fetchMarketName

--- 19_UpSocket/UpSocket/UpSocket/ContentViewModel.swift
+++ 19_UpSocket/UpSocket/UpSocket/ContentViewModel.swift
@@ -10,9 +10,27 @@ import Foundation
 class ContentViewModel {
     
     @Published var marketList: [MarketModel]
+    @Published var marketList: [MarketName]
     
     init(marketList: [MarketModel]) {
         self.marketList = marketList
+    init() {
+        self.marketList = []
+        fetchMarketName()
+    }
+    
+    func fetchMarketName() {
+        NetworkManager.shared.fetchData(api: .marketName, type: [MarketName].self) { response in
+            switch response {
+            case .success(let value):
+                print(value)
+                self.marketList = value
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     }
     
     

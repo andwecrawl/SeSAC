b8590214f1ba1a76cd5fe0c11a3b89d9c56870e0
yeoni <ghj0308@daum.net>
2024-01-04 01:30:14 +0900

    feat: LoginView

--- 19_UpSocket/UpSocket/UpSocket/LoginView.swift
+++ 19_UpSocket/UpSocket/UpSocket/LoginView.swift
@@ -9,17 +9,62 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-    
-        Text("Hello")
-//        Button {
-//            <#code#>
-//        } label: {
-//            <#code#>
-//        }
-
+        ZStack {
+            Color(.mainYellow).ignoresSafeArea()
+            LazyVStack {
+                Image("logo2")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                Spacer(minLength: 10)
+                LoginButtonView()
+            }
+        }
     }
 }
 
 #Preview {
     LoginView()
+}
+
+struct LoginButtonView: View {
+    var body: some View {
+        Group {
+            Button {
+                
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.white)
+                        .frame(width: 330, height: 50)
+                    Text("카카오 로그인")
+                        .foregroundStyle(.black)
+                    
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.white)
+                        .frame(width: 330, height: 50)
+                    Text("애플 로그인")
+                        .foregroundStyle(.black)
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.white)
+                        .frame(width: 330, height: 50)
+                    Text("이메일 로그인")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+    }
 }

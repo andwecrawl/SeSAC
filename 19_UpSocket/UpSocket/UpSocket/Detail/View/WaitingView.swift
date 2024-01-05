7a5001789499b1836c04f6151bd6cf8dc626171f
yeoni <ghj0308@daum.net>
2024-01-06 08:26:27 +0900

    feat: WaitingView

---
+++ 19_UpSocket/UpSocket/UpSocket/Detail/View/WaitingView.swift
@@ -0,0 +1,22 @@
+//
+//  WaitingView.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/6/24.
+//
+
+import SwiftUI
+
+struct WaitingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("죄송해요. 아직 준비 중이에요!")
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    WaitingView()
+}

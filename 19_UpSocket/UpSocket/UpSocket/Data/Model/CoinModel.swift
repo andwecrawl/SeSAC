72fceb0528869f5a4bb01b55de0c2f5b0594b4f7
yeoni <ghj0308@daum.net>
2024-01-05 22:24:24 +0900

    feat: CoinModel

---
+++ 19_UpSocket/UpSocket/UpSocket/Data/Model/CoinModel.swift
@@ -0,0 +1,33 @@
+//
+//  CoinModel.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/5/24.
+//
+
+import Foundation
+
+struct CoinModel: Hashable, Identifiable {
+    let id = UUID()
+    let korName: String
+    let engName: String
+    let marketCode: String
+    var tradePrice: Double
+    var change: Double
+    var accTradePrice: Double
+    
+    mutating func applySocketResponse(response: SocketResponse) {
+        self.tradePrice = response.tradePrice
+        self.accTradePrice = response.accTradePrice
+        self.change = response.change
+    }
+    
+    init(names: MarketName, information: CoinInformation) {
+        self.korName = names.koreanName
+        self.engName = names.englishName
+        self.marketCode = names.market
+        self.tradePrice = information.tradePrice
+        self.change = information.change
+        self.accTradePrice = information.accTradePrice
+    }
+}

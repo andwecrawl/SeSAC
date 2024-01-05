d7c86472a08c1d1cd48cb9e7a69d931193ac42f1
yeoni <ghj0308@daum.net>
2024-01-05 22:24:30 +0900

    feat: ContentViewModel

--- 19_UpSocket/UpSocket/UpSocket/ContentViewModel.swift
+++ 19_UpSocket/UpSocket/UpSocket/ContentViewModel.swift
@@ -6,31 +6,77 @@
 //
 
 import Foundation
+import Combine
 
-class ContentViewModel {
+class ContentViewModel: ObservableObject {
     
-    @Published var marketList: [MarketModel]
-    @Published var marketList: [MarketName]
+    @Published
+    var coinList: [CoinModel] = []
+    private var cancellbae = Set<AnyCancellable>()
     
-    init(marketList: [MarketModel]) {
-        self.marketList = marketList
     init() {
-        self.marketList = []
         fetchMarketName()
     }
     
+    deinit {
+        SocketManager.shared.closeWebSocket()
+    }
+    
     func fetchMarketName() {
-        NetworkManager.shared.fetchData(api: .marketName, type: [MarketName].self) { response in
+        NetworkManager.shared.fetchData(api: .marketName, type: [MarketName].self) { [weak self] response in
             switch response {
             case .success(let value):
-                print(value)
-                self.marketList = value
+                SocketManager.shared.codes = value
+                self?.fetchFeedData()
             case .failure(let error):
                 print(error)
             }
         }
     }
     
+    func fetchFeedData() {
+        
+        NetworkManager.shared.fetchData(api: .currentCharge, type: [CoinInformation].self) { [weak self] result in
+            switch result {
+            case .success(let value):
+                print("success!!")
+                print("======================================================")
+                print(value)
+                
+                var list: [CoinModel] = []
+                for element in value {
+                    if let names = SocketManager.shared.codes.filter({ $0.market == element.market }).first {
+                        list.append(CoinModel(names: names, information: element))
+                    }
+                }
+                self?.coinList = list.filter({ $0.tradePrice > 1 })
+                self?.manageSocket()
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func manageSocket() {
+        
+        SocketManager.shared.openWebSocket()
+        SocketManager.shared.send()
+        
+        SocketManager.shared.data
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] order in
+                print("========================================")
+                guard let self else { return }
+                print(order)
+                for index in self.coinList.indices {
+                    if self.coinList[index].marketCode == order.code {
+                        self.coinList[index].applySocketResponse(response: order)
+                        print("\(coinList[index].korName): has changed!!!!!")
+                    }
+                }
+            }
+            .store(in: &cancellbae)
     }
     
     

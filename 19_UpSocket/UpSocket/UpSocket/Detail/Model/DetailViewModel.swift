18a2c9f46b0530171f7e8ca7c50390017a823dcc
yeoni <ghj0308@daum.net>
2024-01-06 05:31:34 +0900

    feat: DetailView

---
+++ 19_UpSocket/UpSocket/UpSocket/Detail/Model/DetailViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/6/24.
+//
+
+import Foundation
+
+class DetailViewModel: ObservableObject {
+    
+    var coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+    }
+    
+    
+    
+}
---
+++ 19_UpSocket/UpSocket/UpSocket/Detail/View/DetailView.swift
@@ -0,0 +1,17 @@
+//
+//  DetailView.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/6/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @StateObject
+    var viewModel: DetailViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}

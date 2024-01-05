26b538c5a8ae609b785976cd07b93a1a52839954
yeoni <ghj0308@daum.net>
2024-01-05 22:21:41 +0900

    feat: Double To String Extension

---
+++ 19_UpSocket/UpSocket/UpSocket/Data/Extension/Double+.swift
@@ -0,0 +1,26 @@
+//
+//  Double+.swift
+//  UpSocket
+//
+//  Created by yeoni on 1/5/24.
+//
+
+import Foundation
+
+extension Double {
+    func toString() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.roundingMode = .floor
+        numberFormatter.maximumFractionDigits = 3
+        return numberFormatter.string(for: self)!
+    }
+    
+    func toFormattedString() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let cutMillion = Int(self / 1000000)
+        let numStr = numberFormatter.string(for: cutMillion)!
+        return numStr + "백만"
+    }
+}
